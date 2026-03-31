3f3cf405cc1b216c4a7220ee848f3c92ec8bd3e5
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-03-31 14:54:27 -0700

    Creating methods for adding products to cart and decreasing the quantity

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -4,10 +4,33 @@ import { CartContext } from "./CartContext";
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
-  const addToCart = (product) => setCart(prev => [...prev, product]);
+  const addToCart = (dessert) => setCart(prev => {
+    const existe = prev.find(item => item.id === dessert.id);
+    if (existe) {
+      return prev.map(item => {
+        item.id === dessert.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+      })
+    }
+    return [...prev, { ...dessert, quantity: 1 }];
+  });
+
+  const decreaseQuantity = (id) => setCart(prev => {
+    prev.map(item => {
+      item.id === id
+      ? {...item, quantity: item.quantity - 1}
+      : item
+    })
+    .filter(item => item.quantity > 0)
+  })
+
+  const removeFromCart = (id) => {
+    setCart(prev => prev.filter(item => item.id !== id));
+  };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart, decreaseQuantity, removeFromCart }}>
       {children}
     </CartContext.Provider>
   );
