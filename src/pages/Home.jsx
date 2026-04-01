fbc173bc269de4f9fafe80c3b162cb15dc6f5436
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-04-01 11:15:18 -0700

    Styles to cart component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Desserts from '../components/Desserts'
 
 function Home() {
   return (
-    <section className='min-h-screen px-6 py-8 bg-rose-50 flex flex-col items-start xl:flex-row md:gap-8 lg:p-20'>
+    <section className='min-h-screen px-6 py-8 bg-rose-50 flex flex-col items-start  md:gap-8 lg:p-20 xl:flex-row'>
         <Desserts/>
         <Cart/>
     </section>
