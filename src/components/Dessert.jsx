cca4fe4c50eeae196b1b67b59cf8f7673df7a5eb
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-04-07 16:19:20 -0700

    Adding styles to the button that increases and decreases the quantity of desserts in the Dessert component

--- src/components/Dessert.jsx
+++ src/components/Dessert.jsx
@@ -20,29 +20,34 @@ function Dessert({dessert}) {
   
   return (
     <section>
-      <div className='relative mb-8 '>
+      <div className='relative mb-8'>
         <picture>
           <source media="(min-width: 1024px)" srcSet={dessert.image.desktop} />
           <source media="(min-width: 768px)" srcSet={dessert.image.tablet} />
-          <img src={dessert.image.mobile} alt={dessert.name} className='rounded-md w-full object-cover'/>
+          <img src={dessert.image.mobile} alt={dessert.name} className={`rounded-md w-full object-cover ${dessertIsInCart ? "border-2 border-red" : ''}`}/>
         </picture>
         {!dessertIsInCart ? ( 
           <button 
-          className='add-to-cart-btn absolute left-1/2 bottom-0 translate-y-1/2 -translate-x-1/2 w-max whitespace-nowrap group transition cursor-pointer' 
+          className='add-to-cart-btn absolute left-1/2 bottom-0 translate-y-1/2 -translate-x-1/2 whitespace-nowrap group transition cursor-pointer' 
           onClick={() => addToCart(dessert)}>
             <img src={addToCartSvg} alt="" className='h-5 w-5'/>
             <span className='text-preset-4-bold text-rose-900 group-hover:text-red transition'>Add to cart</span>
           </button>
         ): (
-          <button className='cart-quantity absolute left-1/2 bottom-0 translate-y-1/2 -translate-x-1/2  whitespace-nowrap transition'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 10 10" className='border rounded-full border-white p-0.5 cursor-pointer'  onClick={() => addToCart(dessert)}>
-              <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
-            </svg>
+          <div className='cart-quantity absolute left-1/2 bottom-0 translate-y-1/2 -translate-x-1/2  whitespace-nowrap transition'>
+            <button className='h-3.75 w-3.75 p-0.5 flex justify-center items-center border rounded-full border-white cursor-pointer hover:bg-white' onClick={() => removeFromCart(dessert.id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2" className='text-white hover:text-rose-100'>
+                <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z"/>
+              </svg>
+            </button>
             <span className='text-preset-4-bold text-white'>{cart.find(item => item.id === dessert.id)?.quantity || 0}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2" className='border rounded-full border-white cursor-pointer'  onClick={() => removeFromCart(dessert.id)}>
-              <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
-            </svg>
-          </button>
+            <button className='h-3.75 w-3.75 p-0.5 border flex justify-center rounded-full border-white cursor-pointer hover:bg-white '  onClick={() => addToCart(dessert)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10" className='text-white hover:text-rose-100'>
+                <path fill="currentColor" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+              </svg>
+            </button>
+
+          </div>
         )}
 
       </div>
