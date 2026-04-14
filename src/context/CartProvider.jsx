570b1a0b1cfba543ea51829c08c5bbb57e4e7f1f
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-04-14 16:03:02 -0700

    Deleting all desserts instead of just decreasing the quantity of the dessert when clicking the delete button in the dessert list in the cart.

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -18,14 +18,6 @@ export function CartProvider({ children }) {
 
 
 
-  const decreaseQuantity = (id) => setCart(prev => {
-      prev.map(item => {
-        item.id === id
-        ? {...item, quantity: item.quantity - 1}
-        : item
-      })
-      .filter(item =>  item.quantity > 0)
-  })
 
   const removeFromCart = (id) => {
     const dessert = cart.find(item => item.id === id);
@@ -42,12 +34,14 @@ export function CartProvider({ children }) {
     }
   };
 
+  const removeProduct = (id) => setCart(prev => prev.filter(item => item.id !== id));
+
   const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
 
   const clearCart = () => setCart([]);
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, decreaseQuantity, removeFromCart, totalPrice, clearCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, totalPrice, clearCart, removeProduct }}>
       {children}
     </CartContext.Provider>
   );
